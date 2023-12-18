3a15421ef9240d016b6d7affd3a1560c09c71357
Daniel White <dm.white@gmx.com>
2023-12-18 01:58:54 +0000

    Added email normalization test and applied linting fix

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,6 +4,7 @@ Unit Test Suite for Django Models.
 from django.test import TestCase
 from django.contrib.auth import get_user_model as user_model
 
+
 class ModelTests(TestCase):
     """Test Django Models"""
 
@@ -13,11 +14,24 @@ class ModelTests(TestCase):
         email = 'email@test.com'    # Valid dummy email
         pw = 'testpassword1234'     # Valid dummy password
 
-       # Create a user with dummy email and password
+        # Create a user with dummy email and password
         usr = user_model().objects.create_user(
             email=email,
             password=pw,
         )
 
-        self.assertEqual(usr.email, email) # Check if email is correct
-        self.assertTrue(usr.check_password(pw)) # Check if password is correct
+        self.assertEqual(usr.email, email)      # Check if email is correct
+        self.assertTrue(usr.check_password(pw))     # Check if password is correct
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+
+        # Samples check that the domain is lowercased (as per the email spec)
+        sample_emails = [
+            ['testemail1@TEST.COM', 'testemail1@test.com'],
+            ['TestEmail2@Test.com', 'TestEmail2@test.com'],
+        ]
+
+        for testEmail, expectedEmail in sample_emails:
+            usr = user_model().objects.create_user(testEmail, 'test1234')
+            self.assertEqual(usr.email, expectedEmail)
